be18cb67822b07bd9f4506891797067a386f8a48
Stephen <masuod333@gmail.com>
2024-03-26 16:28:02 +0330

    Delete HC-Face-Detector directory

--- HC-Face-Detector/main.py
+++
@@ -1,9 +0,0 @@
-from Module.GUIFaceDetector import GUIFaceDetector as faceDetector
-
-
-webcam_URL = "https://192.168.1.100:8888/video"
-xml = "haarcascade_frontalface_default.xml"
-
-
-if __name__ == "__main__":
-    faceDetector(0).run()
